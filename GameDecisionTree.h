c4f941015fc1f75ff3441c060e99e8ac3fbd2f18
kempringham <kempringham8525@sdsu.edu>
2025-03-26 23:08:56 -0700

    playgame update - checks that userchoice is 1 or 2

--- GameDecisionTree.h
+++ GameDecisionTree.h
@@ -59,23 +59,32 @@ public:
 
         // Create the binary search tree
         for (int i = 1; i < mapLength; i++) {
+            // set the current node to the node at index i
             Node<T>* curr = nodeMap[i];
             Story& story = curr->data;
             int left = story.getLeftEventNumber();
             int right = story.getRightEventNumber();
+
+            // if left child is not -1, set left child to the node at the index of the left child
             if (left != - 1) {
                 curr->left = nodeMap[left];
             }
+            // if left child is -1, set left child to null
             else {
                 curr->left = nullptr;
             }
+
+            // if right child is not -1, set right child to the node at the index of the right child
             if (right != -1) {
                 curr->right = nodeMap[right];
             }
+            // if right child is -1, set right child to null
             else {
                 curr->right = nullptr;
             }
         }
+
+        // Assign root to the first node
         root = nodeMap[1];
 
         // TODO: delete for memory management
@@ -97,33 +106,41 @@ public:
         while (curr != nullptr) {
             // Print the current situation and the paths that the user can take
             cout << curr->data.getDescription() << endl;
+
+            // Print option one
             int option1 = curr->data.getLeftEventNumber();
-            int option2 = curr->data.getRightEventNumber();
             if (option1 != -1) {
                 cout << "1. " << curr->left->data.getDescription() << endl;
             }
+
+            // Print option two
+            int option2 = curr->data.getRightEventNumber();
             if (option2 != -1) {
                 cout << "2. " << curr->right->data.getDescription() << endl;
             }
+
+            // If option one and two are -1, the curr node does not have any children, so the game is over
             if (option1 == -1 & option2 == -1) {
                 cout << "Game Over";
                 break;
             }
 
             // make sure user choice is valid(can only be 1 or 2)
-
-            std:cout << "Enter your choice: ";
             int userChoice;
             while (true) {
+                std:cout << "Enter your choice: ";
                 std::cin >> userChoice;
 
                 if (userChoice == 1 && option1 != -1 || userChoice == 2 && option2 != -1) {
                     break;
                 }
 
+                // If user choice is invalid, output that the user must choose a valid choice
                 if (userChoice != 1 && userChoice != 2) {
                     std::cout << "Must choose either 1 or 2: ";
                 }
+
+                // If user choice is invalid, have user input a valid choice
                 if (std::cin.fail()) {
                     std::cin.clear();
                     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
