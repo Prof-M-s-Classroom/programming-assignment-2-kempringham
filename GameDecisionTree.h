d86709d2b8e1d6a7ca42b9c4fc21520ab81ebf64
kempringham <kempringham8525@sdsu.edu>
2025-03-26 23:51:14 -0700

    Test

--- GameDecisionTree.h
+++ GameDecisionTree.h
@@ -143,7 +143,6 @@ public:
                 // If user choice is invalid, have user input a valid choice
                 if (std::cin.fail()) {
                     std::cin.clear();
-                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
                 }
             }
 
